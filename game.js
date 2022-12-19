2646b36c718220eab0ec1c0aef837220dc524e8f
run <tinuvazhayil@gmail.com>
2022-12-19 05:17:06 -0500

    add more java

--- game.js
+++ game.js
@@ -1,97 +1,100 @@
-   let question = document.querySelector('#question');
-   let choices = Array.from(document.querySelectorAll('.choice-txt'));
-   let progressText = document.querySelector('#progressText');
-   let scoreText = document.querySelector('#score');
-   let progessBarFull = document.querySelector('#progressBarFull');
+// Define a variable to store the current question number, and a variable to store the user's score.
+// Create an array of objects to represent the trivia questions, each containing the question text, a list of possible answers, and the correct answer.
+// Write a function to display the current question and answer choices to the user.
+// Add event listeners to the answer choices to allow the user to select an answer.
+// Write a function to handle the user's selection and check if it is correct. If the answer is correct, increment the user's score and move on to the next question. If the answer is incorrect, display a message to the user and move on to the next question.
+// When the game is finished, display the user's final score to them.
+  
+  
 
 
-   let currentQuestion = {}
-   let answers = true 
-   let score = 0 
-   let questionCounter = 0 
-   let availableQuestions = []
 
 
-   let questions = [
+let questionText = document.querySelector('#questionText');
+let scoreNumber = document.querySelector('#score');
+
+
+
+
+
+
+let answers = true 
+let questionCounter = 0 
+let availableQuestions = []
+
+
+
+
+let score = 0;
+let question = document.querySelector('#question');
+let choices = document.querySelectorAll('.choice-txt')
+
+let questions = [
     {
-        question: "What year was John F. Kennedy assassinated?",
-        choice1: '1963', 
-        choice2: '1955', 
-        choice3: '1972', 
-        choice4: '1965',
-        answer: 1,
+      question: 'What is the capital of France?',
+      choice1: 'Paris',
+      choice2: 'London',
+      choice3: 'Berlin',
+      choice4: 'Rome',
+      correctChoice: 'Paris'
     },
     {
-        question: "Who built the first car in America?",
-        choice1: 'Andrew Carnegie', 
-        choice2: 'Karl Benz', 
-        choice3: 'Henry Ford', 
-        choice4: 'Cornelius Vanderbilt',
-        answer: 3,
+      question: 'What is the highest mountain in the world?',
+      choice1: 'Mount Everest',
+      choice2: 'K2',
+      choice3: 'Kilimanjaro',
+      choice4: 'Denali',
+      correctChoice: 'Mount Everest'
     }
-   ]
+  ]
 
-let SCORE_POINTS = 100
-let MAX_QUESTIONS = 2
-   
+let currentQuestionIndex = 0
+let currentQuestion = questions[currentQuestionIndex] //index of each array
 
-startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-}
-
-getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('/end.html')
-    }
-
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-
-    let questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(choice => {
-        let number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
-
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
-}
-
+  
+question.innerText = currentQuestion.question
+  
 choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
-
-        acceptingAnswers = false 
-        let selectedChoice = e.target
-        let selectedAnswer = selectedChoice.dataset['number']
-
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : "incorrect"
-
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply)
-        
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-        }, 1000)
-    })
+let number = choice.dataset['number']
+    choice.innerText = currentQuestion['choice' + number]
 })
 
-incrementScore = num => {
-    score +=num
-    scoreText.innerText = score
-}
 
-startGame()
+
+choices.forEach(choice => {
+    choice.addEventListener('click', event => {
+      let selectedChoice = event.target.innerText;
+      if (selectedChoice === currentQuestion.correctChoice) {
+        choice.classList.add('correct');
+        // proceed to the next question
+        nextQuestion();
+      } else {
+        // highlight the choice in red for incorrect answer
+        choice.classList.add('incorrect');
+      }
+    });
+  });
+
+
+  function nextQuestion() {
+    // remove the correct or incorrect class from the selected choice
+    choices.forEach(choice => choice.classList.remove('correct', 'incorrect'));
+  
+    // increment the currentQuestionIndex and update the currentQuestion
+    currentQuestionIndex++;
+    currentQuestion = questions[currentQuestionIndex];
+  
+    // update the question and choices on the page
+    question.innerText = currentQuestion.question;
+    choices.forEach(choice => {
+      let number = choice.dataset['number'];
+      choice.innerText = currentQuestion['choice' + number];
+    });
+  }
+ 
+  
+  
+  
+  
+  
+  
